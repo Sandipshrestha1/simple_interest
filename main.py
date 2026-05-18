3e8e1e759c0e6e30fffd168518bc5508abf17d5a
sandipshrestha01 <sandipshrestha2050@gmail.com>
2026-05-18 12:58:37 +0545

    added cors

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from routes.loan_routes import router
 
 app = FastAPI()
+
+# ✅ CORS setup
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # allow all (for development)
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(router)
 
